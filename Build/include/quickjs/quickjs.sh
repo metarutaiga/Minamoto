b486898b11b1fb9d183f86558812fef4567f53bf
TAiGA <>
2024-05-21 19:55:37 +0800

    Update

---
+++ Build/include/quickjs/quickjs.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+# ==============================================================================
+# Minamoto : quickjs Bash
+# 
+# Copyright (c) 2019-2024 TAiGA
+# https://github.com/metarutaiga/minamoto
+# ==============================================================================
+
+if [ ! -f qjsc ]; then
+  clang -DCONFIG_VERSION=\"$(cat VERSION)\" -Ofast cutils.c libbf.c libregexp.c libunicode.c qjsc.c quickjs.c quickjs-libc.c -o qjsc
+fi
+if [ ! -f qjscalc.c ]; then
+  ./qjsc -c -o qjscalc.c -m qjscalc.js
+fi
+if [ ! -f repl.c ]; then
+  ./qjsc -c -o repl.c -m repl.js
+fi
